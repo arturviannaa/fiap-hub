47a4bfbb8d42f69f4366157b1ab3364228ca6279
pedroperrarodev <pedroperrarodev@gmail.com>
2026-08-17 00:15:33 -0300

    atualiza versionCode e versionName para a versão 1.8; refatora script de release para usar sed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "tech.pervian.fiapestudante"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
     }
 
     buildFeatures {
